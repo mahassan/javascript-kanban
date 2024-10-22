5b91d5072882d758f031404d5598038a1fd9a23f
Ammar Hassan <ammar@ammarhassan.co.uk>
2024-10-22 12:14:18 +0100

    source files

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+class kanban {
+    constructor(n) {
+        this.name = 'Board';
+        this.name = n;
+    }
+}
+const board = new kanban('Ammar');
---
+++ app.ts
@@ -0,0 +1,8 @@
+class kanban {
+    name: String = 'Board'
+    constructor(n: String){
+        this.name = n
+    }
+}
+
+const board = new kanban('Ammar')
